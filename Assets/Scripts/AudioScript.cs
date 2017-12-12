28a0b5cc32b3257e8d7e8612bffbb3d727f4ad3c
Vincethevince <31211429+Vincethevince@users.noreply.github.com>
2017-12-12 09:44:37 +0100

    Add sounds to menu
    There are differernt sounds on the "Spawn" and "Despawn" button.
    The "Next" and "Back"-buttons got the same sound.
    
    The sounds are played when you interact with the button.

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+    public AudioClip audioDespawn;
+    public AudioClip audioSpawn;
+    public AudioClip audioMenu;
+    public AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Spawn() { //Sound für Objekt Spawn
+        audioSource.clip = audioSpawn;
+        audioSource.Play();
+    }
+
+    public void Despawn() //Sound für Objekt Despawn
+    {
+        audioSource.clip = audioDespawn;
+        audioSource.Play();
+    }
+
+    public void Menu() //Sound für Menü Switch
+    {
+        audioSource.clip = audioMenu;
+        audioSource.Play();
+    }
+}
