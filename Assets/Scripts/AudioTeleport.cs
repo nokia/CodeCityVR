2e2284f26d9daa836295706704b43ea2dbd4e096
Vincethevince <31211429+Vincethevince@users.noreply.github.com>
2018-01-17 10:45:06 +0100

    Added sound to teleport
    Releasing the touchpad on the secondarry controller will trigger a teleport sound
    
    Problem: The clip is played even if you didn´t teleport.

---
+++ Assets/Scripts/AudioTeleport.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class AudioTeleport : MonoBehaviour {
+
+    public VRTK_ControllerEvents controllerEvents;
+    public AudioClip audioTeleport;
+    public AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        audioSource.clip = audioTeleport;
+	}
+	
+	
+
+    private void OnEnable() {
+        controllerEvents.TouchpadReleased += ControllerEvents_TouchpadReleased;
+    }
+
+    private void OnDisable()
+    {
+        controllerEvents.TouchpadReleased -= ControllerEvents_TouchpadReleased;
+    }
+
+
+    private void ControllerEvents_TouchpadReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        audioSource.Play();
+    }
+}
